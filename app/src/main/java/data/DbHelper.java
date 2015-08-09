52a2d70c28fb2f84a4400a1212684ce5d7cb5c42
murat <delyden@hotmail.com>
2015-08-09 17:02:39 +0300

    add data layer

---
+++ app/src/main/java/data/DbHelper.java
@@ -0,0 +1,7 @@
+package data;
+
+/**
+ * Created by murat on 09/08/2015. flightbook
+ */
+public class DbHelper {
+}
---
+++ app/src/main/java/data/repositories/PilotRepository.java
@@ -0,0 +1,7 @@
+package data.repositories;
+
+/**
+ * Created by murat on 09/08/2015. flightbook
+ */
+public class PilotRepository {
+}
